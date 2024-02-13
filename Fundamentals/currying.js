72f4a40b1ea2e4a40519a364385a5d9f91f3fee4
signaturecoder <snkmr13@gmail.com>
2024-02-13 23:02:27 +0530

    advanced currying implementation

--- Fundamentals/currying.js
+++ Fundamentals/currying.js
@@ -29,7 +29,7 @@ function add(a, b) {
 
 function curry(fn) {
   const noOfArgmofOriginalFn = fn.length;
-  console.log(noOfArgmofOriginalFn);
+  // console.log(noOfArgmofOriginalFn);
   return function curried(...args) {
     if (args.length >= noOfArgmofOriginalFn) {
       return fn.apply(this, args);
@@ -42,11 +42,59 @@ function curry(fn) {
 }
 
 // curry(add);
-const curriedAdd = curry(add);
-console.log(typeof curriedAdd(3));
-curriedAdd(3)(4); // 7
-console.log(curriedAdd(3));
-console.log(curriedAdd(4));
+// const curriedAdd = curry(add);
+// console.log(typeof curriedAdd(3));
+// curriedAdd(3)(4); // 7
+// console.log(curriedAdd(3));
+// console.log(curriedAdd(4));
 
 // const alreadyAddedThree = curriedAdd(3);
 // alreadyAddedThree(4); // 7
+
+
+/**
+ * Implement the `curry` function which accepts a function as only argument
+ * and return a function that accepts `any no of arguments` and 
+ * returns a function which can be repeatedly called until at least the minimum no of arguments has been provided
+ * The initial fn argument is then invoked with the provided arguments. 
+ */
+
+function addTwo(a, b) {
+  return a + b;
+}
+
+function currySpecial(fn) {
+  const noOfFnParamenters = fn.length;
+  return function curried(...args) {
+    // comaparision should not be greater than the noofparameters
+    if (args.length >= noOfFnParamenters) {
+      return fn.apply(this, args);
+    }
+
+    // here arg === undefined case is redundant.
+    return (...arg) =>
+      arg === undefined
+        ? curried.apply(this, args)
+        : curried.apply(this, [...args, ...arg]);
+  };
+}
+
+const curriedAddTwo = currySpecial(addTwo);
+console.log(curriedAddTwo(3)(4)); // 7
+console.log(curriedAddTwo(3, 4)); // 7
+const alreadyAddedThree = curriedAddTwo(3);
+console.log(alreadyAddedThree(4)); // 7
+
+
+function multiplyThree(a, b, c) {
+  return a * b * c;
+}
+const curriedMultiplyThree = currySpecial(multiplyThree);
+console.log(curriedMultiplyThree(4)(5)(6)); // 120
+console.log(curriedMultiplyThree(4)(5, 6)); // 120
+console.log(curriedMultiplyThree(4, 5)(6)); // 120
+console.log(curriedMultiplyThree(4, 5, 6)); // 120
+
+const containsFour = curriedMultiplyThree(4);
+const containsFourMulFive = containsFour(5);
+console.log(containsFourMulFive(6)); // 120
